709549342825576fdee5f5640915b430f546717f
phureekps-ops <phureekps@gmail.com>
2026-07-21 10:54:55 +0700

    Delete chemlink-backend/chemlReRemove nested chemlink-backend duplicate foldermove nested chemlink-backend duplicate folderink-backend directory

--- chemlink-backend/chemlink-backend/src/modules/products/dto/update-product.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { IsBoolean, IsOptional } from 'class-validator';
-import { CreateProductDto } from './create-product.dto';
-
-export class UpdateProductDto extends PartialType(CreateProductDto) {
-  @IsOptional()
-  @IsBoolean()
-  isPublished?: boolean;
-}
--- chemlink-backend/chemlink-backend/src/modules/rfq/dto/update-rfq.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateRfqDto } from './create-rfq.dto';
-
-export class UpdateRfqDto extends PartialType(CreateRfqDto) {}
